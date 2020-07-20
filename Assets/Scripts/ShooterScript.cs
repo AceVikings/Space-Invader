878da80085a5d79dfeae08af02fd5911dcc6cc72
akashjha011 <akash.jsmith@gmail.com>
2020-07-21 01:45:15 +0530

    Added Muzzle Flash
    Added Particle System for Muzzle Flash , still a little rough but works

--- Assets/Scripts/ShooterScript.cs
+++ Assets/Scripts/ShooterScript.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ShooterScript : MonoBehaviour
 {
     public Camera Cam;
-
+    public ParticleSystem muzzleFlash;
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -17,10 +17,17 @@ public class ShooterScript : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hit;
         if(Physics.Raycast(Cam.transform.position,Cam.transform.forward,out hit))
         {
             Debug.Log(hit.transform.name);
+
+            EnemyDamage enemy = hit.transform.GetComponent<EnemyDamage>();
+            if (enemy != null)
+            {
+                enemy.Die();
+            }
         }
     }
 }
