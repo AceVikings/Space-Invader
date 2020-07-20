8db0a9bea0c11036866cf741eb289b05f5af4ea4
akashjha011 <akash.jsmith@gmail.com>
2020-07-21 01:06:30 +0530

    Added Shooter Script

---
+++ Assets/Scripts/ShooterScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    public Camera Cam;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Cam.transform.position,Cam.transform.forward,out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
